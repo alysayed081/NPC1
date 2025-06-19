3addb91b42b239d0293a519a5fa375447a0705ed
alysayed081 <alysayed081@gmail.com>
2025-06-19 09:00:54 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -43,3 +43,75 @@ function startCount(el) {
     }
   }, 2000 / goal);
 }
+
+
+var imgBox = document.querySelectorAll(".card-img");
+var pop = document.querySelector(".pop");
+var popBox = document.querySelector(".pop-box");
+var prevArrow = document.querySelector("#prev");
+var nextArrow = document.querySelector("#next");
+var closBtn = document.querySelector("#close");
+
+var curIndex;
+
+for (let i = 0; i < imgBox.length; i++) {
+  imgBox[i].addEventListener("click", function (e) {
+    curIndex = i;
+    let imgSrc = e.currentTarget.querySelector("img").getAttribute("src");
+    pop.classList.replace("d-none", "d-flex");
+
+    popBox.style.setProperty("background-image", `url(${imgSrc})`);
+  });
+}
+
+function nextImg() {
+  curIndex++;
+
+  if (curIndex >= imgBox.length) {
+    curIndex = 0;
+    let imgSrc = imgBox[curIndex].querySelector("img").getAttribute("src");
+    console.log(imgSrc);
+    popBox.style.setProperty("background-image", `url(${imgSrc})`);
+  } else {
+    let imgSrc = imgBox[curIndex].querySelector("img").getAttribute("src");
+    console.log(imgSrc);
+    popBox.style.setProperty("background-image", `url(${imgSrc})`);
+  }
+}
+
+nextArrow.addEventListener("click", function () {
+  nextImg();
+});
+
+function prevImg() {
+  curIndex--;
+
+  if (curIndex < 0) {
+    curIndex = imgBox.length - 1;
+    let imgSrc = imgBox[curIndex].querySelector("img").getAttribute("src");
+    popBox.style.setProperty("background-image", `url(${imgSrc})`);
+  } else {
+    let imgSrc = imgBox[curIndex].querySelector("img").getAttribute("src");
+    popBox.style.setProperty("background-image", `url(${imgSrc})`);
+  }
+}
+
+prevArrow.addEventListener("click", function () {
+  prevImg();
+});
+
+closBtn.addEventListener("click", function () {
+  closeButton();
+});
+
+function closeButton() {
+  pop.classList.replace("d-flex", "d-none");
+}
+
+pop.addEventListener("click", function () {
+  closeButton();
+});
+
+popBox.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
